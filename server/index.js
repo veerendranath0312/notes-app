65d6b1ef78e08ca4677205c18c3281fd2754db6f
Veerendranath <veerendranath0312@gmail.com>
2026-05-29 15:33:46 -0400

    connect database to backend

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const Note = require('./models/note.js')
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -9,30 +11,32 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
-let notes = [
-  {
-    id: '1',
-    content: 'HTML is easy',
-    important: true,
-  },
-  {
-    id: '2',
-    content: 'Browser can execute only JavaScript',
-    important: false,
-  },
-  {
-    id: '3',
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-]
+// let notes = [
+//   {
+//     id: '1',
+//     content: 'HTML is easy',
+//     important: true,
+//   },
+//   {
+//     id: '2',
+//     content: 'Browser can execute only JavaScript',
+//     important: false,
+//   },
+//   {
+//     id: '3',
+//     content: 'GET and POST are the most important methods of HTTP protocol',
+//     important: true,
+//   },
+// ]
 
 app.get('/', (req, res) => {
   res.status(200).send('<h1>hello world!</h1>')
 })
 
 app.get('/api/notes', (req, res) => {
-  res.status(200).json(notes)
+  Note.find({}).then((notes) => {
+    res.status(200).json(notes)
+  })
 })
 
 app.post('/api/notes', (req, res) => {
@@ -44,26 +48,22 @@ app.post('/api/notes', (req, res) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: crypto.randomUUID(),
-  }
+  })
 
-  notes = [...notes, note]
-
-  res.status(201).json(note)
+  note.save().then((savedNote) => {
+    res.status(201).json(savedNote)
+  })
 })
 
 app.get('/api/notes/:id', (req, res) => {
   const { id } = req.params
-  const note = notes.find((note) => note.id === id)
 
-  if (note) {
+  Note.findById(id).then((note) => {
     res.status(200).json(note)
-  } else {
-    res.status(404).json({ message: `note not found` })
-  }
+  })
 })
 
 app.delete('/api/notes/:id', (req, res) => {
---
+++ server/models/note.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+mongoose
+  .connect(url, { family: 4 })
+  .then((result) => console.log('connected to mongodb'))
+  .catch((error) =>
+    console.log(`error connecting to mongodb: ${error.message}`)
+  )
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+module.exports = Note
